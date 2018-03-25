36a6b963402bf8575db6675e4ce03484145c8718
vpdrnls <gas0129@naver.com>
2018-03-25 16:21:57 +0900

    모바일 터치 변경

--- Pilot-master/Assets/Scripts/InputManager.cs
+++ Pilot-master/Assets/Scripts/InputManager.cs
@@ -6,54 +6,76 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-	enum PUSH
-	{
-		Begin,
-		Pressing,
-	}
+	//enum PUSH
+	//{
+	//	Begin,
+	//	Pressing,
+	//}
 	public static Vector2 MoveDir;
 	public RectTransform JoyFront;
 	public RectTransform JoyBack;
 	public float DisBetweenTwo;
 	public float currentDis;
 	
-	private PUSH mousePush;
+	//private PUSH mousePush;
 
 	private void Start()
 	{
-		mousePush = PUSH.Begin;
+	//	mousePush = PUSH.Begin;
 	}
 	private void Update()
 	{
 
 #if UNITY_EDITOR
-		if (Input.GetMouseButton(0))
-		{
-			Vector2 convertPos = Input.mousePosition;
+		//if (Input.GetMouseButton(0))
+		//{
+		
+		//	Vector2 convertPos = Input.mousePosition;
+		//	if (mousePush == PUSH.Begin)
+		//	{
+		//		JoyBack.position = convertPos;
+		//		JoyStickInit(true);
+		//	}
+		//	if(mousePush == PUSH.Pressing)
+		//	{
+		//		if (DistanceCheck() == true)
+		//			JoyFront.position = convertPos;
+		//		if (DistanceCheck() == false)
+		//			JoyFront.position = (Vector2)JoyBack.position + (convertPos - (Vector2)JoyBack.position).normalized * DisBetweenTwo;
 
-			if (mousePush == PUSH.Begin)
+		//		MoveDir = (JoyFront.position - JoyBack.position).normalized;
+		//	}
+		//}
+		//if (Input.GetMouseButtonUp(0))
+		//{
+		//	JoyStickInit(false);
+		//}
+		if (Input.touchCount > 0)
+		{
+			Vector2 convertPos = Input.GetTouch(0).position;
+
+			if (Input.GetTouch(0).phase == TouchPhase.Began)
 			{
 				JoyBack.position = convertPos;
+				JoyFront.position = convertPos;
 				JoyStickInit(true);
 			}
+			if (Input.GetTouch(0).phase == TouchPhase.Moved)
+			{
+				if (DistanceCheck() == true)
+					JoyFront.position = convertPos;
+				if (DistanceCheck() == false)
+					JoyFront.position = (Vector2)JoyBack.position + (convertPos - (Vector2)JoyBack.position).normalized * DisBetweenTwo;
 
-			if(DistanceCheck() == true)
-				JoyFront.position = convertPos;
-			if(DistanceCheck() == false)
-				JoyFront.position = (Vector2)JoyBack.position + (convertPos - (Vector2)JoyBack.position).normalized * DisBetweenTwo;
-
-			MoveDir = (JoyFront.position - JoyBack.position).normalized;
-		}
-		if(Input.GetMouseButtonUp(0))
-		{
-			JoyStickInit(false);
+				MoveDir = (JoyFront.position - JoyBack.position).normalized;
+			}
+			if (Input.GetTouch(0).phase == TouchPhase.Ended)
+			{
+				JoyStickInit(false);
+			}
 		}
 #elif UNITY_ANDROID
-		if(Input.touchCount > 0)
-		{
-			
-
-		}
+		
 #endif
 	}
 	private void JoyStickInit(bool isActive)
@@ -62,14 +84,14 @@ public class InputManager : MonoBehaviour
 		{
 			JoyBack.gameObject.SetActive(true);
 			JoyFront.gameObject.SetActive(true);
-			mousePush = PUSH.Pressing;
+			//mousePush = PUSH.Pressing;
 		}
 		else
 		{
 			JoyBack.gameObject.SetActive(false);
 			JoyFront.gameObject.SetActive(false);
 			MoveDir = Vector2.zero;
-			mousePush = PUSH.Begin;
+			//mousePush = PUSH.Begin;
 		}
 	}
 	private bool DistanceCheck()
